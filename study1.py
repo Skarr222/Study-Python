056d6dc29c4ade18b03e9738d505195055569664
Oskar Gomza <oskargomza54@gmail.com>
2022-05-31 22:33:55 +0200

    some sort of exercises

--- study1.py
+++ study1.py
@@ -1,13 +1,46 @@
-x = 3              # a whole number
-f = 3.1415926      # a floating point number
-name = "Python"    # a string
+# The example below shows several variables.
+# x = 3              # a whole number
+# f = 3.1415926      # a floating point number
+# name = "Python"    # a string
 
+# print(x)
+# print(f)
+# print(name)
+
+#combination = name + " " + name
+# print(combination)
+
+#sum = f + f
+# print(sum)
+
+
+# Naming
+#x = 3
+#X = 4
+# print(x)
+# print(X)
+
+from itertools import combinations
+
+# program that solves and shows the summation of 64 + 32.
+#x = 64
+#y = 32
+#sum = x + y
+# print(sum)
+
+x = "Nancy"
 print(x)
-print(f)
-print(name)
 
-combination = name + " " + name
-print(combination)
+# Combine numbers and text
+s = "My lucky number is %d, what is yours?" % 7
+print(s)
 
-sum = f + f
-print(sum)
+# alternative method of combining numbers and text
+s = "My lucky number is " + str(7) + ", what is yours?"
+print(s)
+
+# print character by index
+print(x[0])
+
+# print piece of string
+print(x[0:3])
